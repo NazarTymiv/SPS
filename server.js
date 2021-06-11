16b7a1605a95bb23063adb72e481a5c82204c37e
NazarTymiv <50737309+NazarTymiv@users.noreply.github.com>
2021-06-11 16:10:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ io.on('connection', socket => {
     })
 })
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`Server runing on port ${PORT}`);
